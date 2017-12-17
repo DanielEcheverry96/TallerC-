3a2692e718beb94636cd248e0b19157c65877bca
DanielEcheverry96 <dfecheverryg@unal.edu.co>
2017-12-17 17:45:23 -0500

    Version 4.0

---
+++ TallerC/TallerC/Controllers/NumerosConsecutivosController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TallerC.Models;
+
+namespace TallerC.Controllers
+{
+    public class NumerosConsecutivosController : Controller
+    {
+        string mensaje = "";
+
+        // GET: NumerosConsecutivos
+        public ActionResult Index(NumerosConsecutivos numconsetemp)
+        {
+            int ini = numconsetemp.inicio;
+            int final = numconsetemp.fin;
+            string resultado = "";
+            if (ini <= final)
+            {
+                resultado = inicio(ini,final);
+            }
+            else
+            {
+                resultado = fin(ini, final);
+            }
+
+            ViewBag.Resultado = resultado;
+            return View();
+        }
+
+        public string inicio(int inicio, int fin)
+        {
+            for (int i = inicio; i <= fin; i++)
+            {
+                mensaje += i + ", ";
+            }
+            return mensaje;
+        }
+
+        public string fin(int fin, int inicio)
+        {
+            for (int i = fin; i >= inicio; i--)
+            {
+                mensaje += i + ", ";
+            }
+            return mensaje;
+        }
+    }
+}
---
+++ TallerC/TallerC/Models/NumerosConsecutivos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TallerC.Models
+{
+    public class NumerosConsecutivos
+    {
+        public int inicio { get; set; }
+        public int fin { get; set; }
+    }
+}
