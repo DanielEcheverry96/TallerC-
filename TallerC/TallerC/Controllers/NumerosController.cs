d964c8fe3604f34eb4c4ff30e3e4cf3d3b841c02
DanielEcheverry96 <dfecheverryg@unal.edu.co>
2017-12-17 17:55:54 -0500

    Version 5.0

---
+++ TallerC/TallerC/Controllers/NumerosController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TallerC.Models;
+
+namespace TallerC.Controllers
+{
+    public class NumerosController : Controller
+    {
+        // GET: Numeros
+        public ActionResult Index(Numeros numerostemp)
+        {
+            int numero1 = numerostemp.numero1, numero2 = numerostemp.numero2, resultado = 0;
+
+            if (numero1 < numero2)
+            {
+                resultado = numero1 + numero2;
+            }
+            else if (numero1 > numero2)
+            {
+                resultado = numero1 - numero2;
+            }
+            else
+            {
+                resultado = numero1 * numero2;
+            }
+
+            ViewBag.Resultado = resultado;
+            return View();
+        }
+    }
+}
---
+++ TallerC/TallerC/Models/Numeros.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TallerC.Models
+{
+    public class Numeros
+    {
+        public int numero1 { get; set; }
+        public int numero2 { get; set; }
+    }
+}
