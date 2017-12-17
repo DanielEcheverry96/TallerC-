02f193dd540dc7d44c8472ca49fe606779e9f86c
DanielEcheverry96 <dfecheverryg@unal.edu.co>
2017-12-17 17:22:30 -0500

    Version 3.0

---
+++ TallerC/TallerC/Controllers/PesoRecomendadoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TallerC.Models;
+
+namespace TallerC.Controllers
+{
+    public class PesoRecomendadoController : Controller
+    {
+        // GET: PesoRecomendado
+        public ActionResult Index(PesoRecomendado pesotemp)
+        {
+
+            ViewBag.PesoRecomendado = (pesotemp.altura - 100 + pesotemp.edad / 10) * 0.9;
+            return View();
+        }
+    }
+}
---
+++ TallerC/TallerC/Models/PesoRecomendado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TallerC.Models
+{
+    public class PesoRecomendado
+    {
+        public int altura { get; set; }
+        public int edad { get; set; }
+    }
+}
