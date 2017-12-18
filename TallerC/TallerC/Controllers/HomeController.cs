08c928ec4eb3c68ea08b10bc08d41451d163f6fe
DanielEcheverry96 <dfecheverryg@unal.edu.co>
2017-12-17 19:13:03 -0500

    Version 5.1.1

--- TallerC/TallerC/Controllers/HomeController.cs
+++ TallerC/TallerC/Controllers/HomeController.cs
@@ -13,19 +13,6 @@ namespace TallerC.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
 
         public ActionResult IndexFiguras()
         {
